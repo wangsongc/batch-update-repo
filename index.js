b756f443d6028a453c8e757795a443ad32b34182
wangsongc <wangsongc@foxmail.com>
2022-04-19 11:18:13 +0800

    v1.3.8

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-// const github = require('@actions/github');
 const core = require('@actions/core');
 const { exec } = require('@actions/exec');
+const { url } = require('inspector');
 const { URL } = require('url');
 
-// try {
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput('who-to-greet');
-//   console.log(`Hello ${nameToGreet}!`);
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
+const getProjectName = (repoUrl) => {
+  return repoUrl.match(/(?<=\/)[^\/]+(?=\.git)/)[0];
+};
 
+function generateURL(repoUrl, token) {
+  const url = new URL(repoUrl);
+  url.username = 'oauth2';
+  url.password = token;
+  return url.toString();
+}
+
+const token = core.getInput('github_token', { required: true }).trim();
 const sourceRepos = core
   .getInput('source_repo', { required: true })
   .trim()
-  .split(',');
+  .split(',')
+  .map((item) => {
+    return generateURL(item, token);
+  });
 const targetRepos = core
   .getInput('target_repo', { required: true })
   .trim()
-  .split(',');
-const token = core.getInput('github_token', { required: true }).trim();
+  .split(',')
+  .map((item) => {
+    return generateURL(item, token);
+  });
 
 const source_tags = core
   .getInput('source_tag', { required: true })
@@ -32,53 +36,44 @@ const source_tags = core
   .split(',');
 const source_branchs = core.getInput('source_branch').trim().split(',');
 
-const generateURL = (repoUrl, token) => {
-  const url = new URL(repoUrl);
-  url.username = 'oauth2';
-  url.password = token;
-  return url.toString();
-};
-
-const workdir = 'work';
+const project_names = sourceRepos.map((item) => {
+  return getProjectName(item);
+});
 
 async function run() {
   /**
- * 同步代码到目标仓库，并创建tag分支
-git clone https://github.com/wangsongc/myaction.git -b main
-cd myaction
-git checkout -b v1.1 --depth=1
-git push --mirror https://github.com/wangsongc/target.git
+ * git clone https://oauth2:${token}@github.com/vuejs/core.git -b main core
+cd core
+git remote set-url origin https://oauth2:${token}@github.com/wangsongc/vue-next.git
+git checkout v3.2.31
+git push origin HEAD:main
+ * 
  */
   try {
     for (let index = 0; index < sourceRepos.length; index++) {
       await exec('git', [
         'clone',
         `--branch=${source_branchs[index]}`,
-        generateURL(sourceRepos[index], token),
-        workdir
+        sourceRepos[index],
+        `${project_names[index]}`
       ]);
 
-      await exec('git', ['checkout', `-b ${source_tags[index]} --depth=1`], {
-        cwd: `./${workdir}`
+      await exec('git', ['remote', `set-url origin ${targetRepos[index]}`], {
+        cwd: `./${project_names[index]}`
       });
 
-      await exec(
-        'git',
-        ['push', '--mirror', generateURL(targetRepos[index], token)],
-        {
-          cwd: `./${workdir}`
-        }
-      );
-    }
+      await exec('git', ['checkout', `${source_tags[index]}`], {
+        cwd: `./${project_names[index]}`
+      });
 
-    // if (!destRepo.startsWith('https://')) {
-    //   throw new Error('only support https repo type now')
-    // }
+      await exec('git', ['push', `origin HEAD:${source_branchs[index]}`], {
+        cwd: `./${project_names[index]}`
+      });
+    }
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
   }
-  //
 }
 
 run();
